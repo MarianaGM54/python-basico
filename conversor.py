a24254df07cdd55e0fb5ddf6c2f4177f5863751b
Mariana Gallego <marianagmuneton@gmail.com>
2022-11-22 23:43:43 -0500

    primer commmit a conversor

---
+++ conversor.py
@@ -0,0 +1,6 @@
+pesos = input('cuantos pesos colombianos tienes?: ')
+pesos = float(pesos)
+valor_dolar = 5000
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+print('Tienes $' + dolares + ' dolares')
